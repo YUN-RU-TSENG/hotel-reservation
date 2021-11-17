992bbe5c1573310111068f96f7115cf39cbba0d8
TSENG-YUN-RU <coco002197@gmail.com>
2021-11-17 13:48:43 +0800

    f

--- src/composables/roomPages/useRoomGuest.js
+++ src/composables/roomPages/useRoomGuest.js
@@ -1,6 +1,6 @@
 import { computed } from '@vue/reactivity'
 
-export function useRoomGuest(room) {
+export default function useRoomGuest(room) {
     const roomGuest = computed(() =>
         room.descriptionShort.GuestMin === room.descriptionShort.GuestMax
             ? room.descriptionShort.GuestMin
